5f51264f34f767ad2776344b14867aa2f3b53810
AnanduVijay <anandu.fc@gmail.com>
2024-10-11 13:55:22 +0530

    randomColor button completed

--- src/screen/randomColor/RandomColor.js
+++ src/screen/randomColor/RandomColor.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 
 const RandomColor = () => {
+  const [bgColor, setBgColor] = useState('#000');
+  const generateColor = () => {
+    const colorRandom = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${colorRandom}`;
+  };
+  const handlePress = () => {
+    const newColor = generateColor();
+    setBgColor(newColor);
+  };
   return (
-    <View>
-      <Text>RandomColor</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: bgColor}]}
+        onPress={() => handlePress()}>
+        <Text style={styles.buttonText}>Touch Me!!</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default RandomColor
+export default RandomColor;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  button: {
+    padding: 15,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
